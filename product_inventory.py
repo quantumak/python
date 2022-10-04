9cb7d53b84f013bf5b62136898b9550d0dec1c4d
Daniel Romero <danieljromero305@gmail.com>
2022-10-03 21:33:55 -0400

    Fixed most of the spacing errors

--- product_inventory.py
+++ product_inventory.py
@@ -12,50 +12,50 @@ popval = []
 #first product function
 print("Name of product 1: ", end = "")
 productlist.append(input())
-print("Price of the product: ")
+print("Price of the product: ", end='')
 pricelist.append(float(input()))
 
-print("Inventory after the 1st product is added to it:")
+print("\nInventory after the 1st product is added to it:")
 print(productlist,pricelist)
 print("")
 #--------------------------------------------------------------------------------------
 #second product function
 print("Name of product 2 to add to the end: ", end = "")
 productlist.append(input())
-print("Price of the product: ")
+print("Price of the product: ",end='')
 pricelist.append(float(input()))
 
-print("Inventory after the 2nd product is added to the end:")
+print("\nInventory after the 2nd product is added to the end:")
 print(productlist,pricelist)
 print("")
 #--------------------------------------------------------------------------------------
 #third product function
 print("Name of product 3 to add after the 1st product: ", end = "")
 productlist.insert(1 , (input()))
-print("Price of the product: ")
+print("Price of the product: ",end='')
 pricelist.insert(1 , (float(input())))
 
-print("Inventory after the 3rd product is added after the 1st product:")
+print("\nInventory after the 3rd product is added after the 1st product:")
 print(productlist,pricelist)
 print("")
 #--------------------------------------------------------------------------------------
 #fourth product function
 print("Name of product 4 to insert in position 3 of inventory: ", end = "")
 productlist.insert(2, (input()))
-print("Price of the product: ")
+print("Price of the product: ",end='')
 pricelist.insert(2 , (float(input())))
 
-print("Inventory after the 4th product is added to position 3:")
+print("\nInventory after the 4th product is added to position 3:")
 print(productlist,pricelist)
 print("")
 #--------------------------------------------------------------------------------------
 #fifth product function
 print("Name of product 5 to add to the end of the inventory: ", end = "")
 productlist.append(input())
-print("Price of the product: ")
+print("Price of the product: ",end='')
 pricelist.append(float(input()))
 
-print("Inventory after the 5th product is added to the end of inventory:")
+print("\nInventory after the 5th product is added to the end of inventory:")
 print(productlist,pricelist)
 print("")
 #--------------------------------------------------------------------------------------
